c39b4801adcdff0cbb5870073a68e6dfd801d503
Jinwook Baek <799696+kokospapa8@users.noreply.github.com>
2020-04-20 09:51:50 +0900

    Euclid distance, 원의 접점의 수

--- baekjoon/1002.py
+++ baekjoon/1002.py
@@ -6,10 +6,8 @@ for _ in range(no_case):
   x1, y1, r1, x2, y2, r2 = map(int,input().split())
   t1 = (x1,y1)
   t2 = (x2,y2)
-
   euclid_distance = math.sqrt(sum([(a - b) ** 2 for a, b in zip(t1, t2)]))
-
-  if r2 > r1:
+  if r1 < r2:
     r1, r2 = r2, r1
 
   if r1 > euclid_distance or r2 > euclid_distance:
