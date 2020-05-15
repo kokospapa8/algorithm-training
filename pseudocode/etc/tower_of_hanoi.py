ddfcb58f7ffb0e657bf641475f69f93300f093ed
Jinwook Baek <799696+kokospapa8@users.noreply.github.com>
2020-05-15 15:32:42 +0900

    Update tower_of_hanoi.py

--- pseudocode/etc/tower_of_hanoi.py
+++ pseudocode/etc/tower_of_hanoi.py
@@ -1,4 +1,3 @@
-
 # time complexity : 2**n -1
 # https://mathworld.wolfram.com/TowerofHanoi.html
 
@@ -7,5 +6,15 @@ def hanoi(n, from_pos, to_pos, aux_pos):
         print(from_pos, "->", to_pos)
         return
     hanoi(n - 1, from_pos, aux_pos, to_pos)
-    print(from_pos, "->", to_pos)
+    print(n, from_pos, "->", to_pos)
     hanoi(n - 1, aux_pos, to_pos, from_pos)
+
+
+/*
+A, B, C :: n disks
+        
+1) move n-1 From A to B through C 
+2) move nth disk from A to C
+3) move n-1 disks to C through A
+
+*/
